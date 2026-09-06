2b4aeaf211e015d044aa9c85400a0582fdfc27df
Basem Esam <basem.esam.omar@gmail.com>
2026-09-07 02:13:11 +0300

    fix: replace hardcoded allowed hosts with trustedHosts utility for file URL validation

--- src/validations/assignmentSubmission.validation.js
+++ src/validations/assignmentSubmission.validation.js
@@ -20,19 +20,7 @@ exports.assignmentStudentIdSchema = Joi.object({
 
 // Same trusted-host allowlist as src/utils/attachmentValidation.js, applied
 // to a single URL instead of an array (a submission has exactly one file).
-const ALLOWED_HOSTS = [
-  'drive.google.com',
-  'docs.google.com',
-  'dropbox.com',
-  'dl.dropboxusercontent.com',
-  'github.com',
-  'raw.githubusercontent.com',
-  'res.cloudinary.com',
-  'i.imgur.com',
-  'imgur.com',
-  'cdn.discordapp.com',
-  'media.discordapp.net',
-];
+const ALLOWED_HOSTS = require('../utils/trustedHosts');
 
 const fileUrlSchema = Joi.string()
   .uri()
